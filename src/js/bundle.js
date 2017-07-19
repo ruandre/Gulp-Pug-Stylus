e985176c4fcf620e19f525a61569e652b7c609e1
Ruandre <ruandrer@bidvestdata.co.za>
2017-07-19 16:33:40 +0200

    Add library via Bower (example)

--- src/js/bundle.js
+++ src/js/bundle.js
@@ -1,4 +1,17 @@
 /**
- * @requires lib/one.js
- * @requires lib/two.js
+ * @requires ../../bower_components/zepto/zepto.min.js
+ * @requires ./lib/one.js
+ * @requires ./lib/two.js
  */
+
+// dom ready
+document.addEventListener('DOMContentLoaded', function (event) {
+  (function ($) {
+    'use strict'
+
+    $('body').prepend('<p id="test"></p>')
+    $('#test').text('Works!')
+
+  // assign available library to $
+  })(Zepto || jQuery)
+})
